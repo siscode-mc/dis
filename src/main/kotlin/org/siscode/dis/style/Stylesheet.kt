32ff65a40860ba57307a976fd7b6beeb1ef564a5
roridev <t3ctotalmenterandom1@outlook.com>
2021-12-02 22:57:36 -0300

    Add `MEMBERSTYLE` style

--- src/main/kotlin/org/siscode/dis/style/Stylesheet.kt
+++ src/main/kotlin/org/siscode/dis/style/Stylesheet.kt
@@ -1,5 +1,8 @@
 package org.siscode.dis.style
 
+import dev.kord.core.entity.Member
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.toList
 import net.minecraft.text.Style
 import net.minecraft.util.Formatting
 
@@ -11,5 +14,10 @@ class Stylesheet {
         val INFO : Style = EMPTY.withColor(Formatting.GRAY)
         val TIP : Style = EMPTY.withColor(Formatting.GOLD).withFormatting(Formatting.ITALIC)
         val TRUE : Style = EMPTY.withColor(Formatting.GREEN)
+
+        suspend fun MEMBERSTYLE (m : Member) : Style {
+            val toprole = m.roles.filterNot { it.color.rgb == 0x0 }.toList().maxOrNull()
+            return EMPTY.withColor(toprole?.color?.rgb ?: 0xFFFFFF)
+        }
     }
 }
