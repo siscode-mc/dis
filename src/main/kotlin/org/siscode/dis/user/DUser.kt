09dcae2570f9f1d97573b1bba4d8ac62c235f9ca
roridev <t3ctotalmenterandom1@outlook.com>
2021-12-08 19:20:34 -0300

    Add join-kind secondary constructor to `DLinkedUser`

--- src/main/kotlin/org/siscode/dis/user/DUser.kt
+++ src/main/kotlin/org/siscode/dis/user/DUser.kt
@@ -6,4 +6,12 @@ sealed interface DUser
 
 sealed class DDiscordUser(val id: ULong, val name: String) : DUser
 sealed class DMinecraftUser(val uuid : UUID, val name: String) : DUser
-sealed class DLinkedUser(val uuid: UUID, val discordId: ULong, val minecraftName : String, val discordName : String) : DUser
+sealed class DLinkedUser(val uuid: UUID, val discordId: ULong, val minecraftName : String, val discordName : String) : DUser {
+    // Join-Kind constructor
+    constructor(discordUser: DDiscordUser, minecraftUser: DMinecraftUser) : this(
+        minecraftUser.uuid,
+        discordUser.id,
+        minecraftUser.name,
+        discordUser.name
+    )
+}
